0bef30cab1b9b23a2116b664cf34fc4c9e1ebeaf
Wupinyin <admin@wupinyin.co.uk>
2026-02-20 09:55:58 +0000

    Bug: introduced trying to solve benchmark string.Builder error

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -3,9 +3,9 @@ package iteration
 const repeatCount = 5
 
 func Repeat(character string) string {
-	var repeated string
+	var repeated string.Builder
 	for i := 0; i < repeatCount; i++ {
-		repeated = repeated + character
+		repeated.WriteSstring(character)
 	}
-	return repeated
+	return repeated.String()
 }
