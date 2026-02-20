1da969e24fb62e8747579d7ce1c91da27974c1d3
Wupinyin <admin@wupinyin.co.uk>
2026-02-20 11:04:27 +0000

    bugfix: strings import missing

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,11 +1,13 @@
 package iteration
 
+import "strings"
+
 const repeatCount = 5
 
 func Repeat(character string) string {
-	var repeated string.Builder
+	var repeated strings.Builder
 	for i := 0; i < repeatCount; i++ {
-		repeated.WriteSstring(character)
+		repeated.WriteString(character)
 	}
 	return repeated.String()
 }
